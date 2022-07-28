94db1624bdc0bc6a264a5599fba87fba0168eecb
Aaron Fowlkes <afowlkes21@gmail.com>
2022-07-28 11:08:41 -0400

    post added

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -8,8 +8,8 @@ function ListingsContainer( {dataList, setDataList, keepData} ) {
   const handleSortClick = () => {
     console.log(sorting)
     if (sorting) {
-      // let dat = [...dataList]
-      setDataList(dataList.sort((a,b) => {
+      let dat = [...dataList]
+      setDataList(dat.sort((a,b) => {
         return (a.location < b.location ? -1 : 1)
       }))
     } else {
@@ -19,9 +19,34 @@ function ListingsContainer( {dataList, setDataList, keepData} ) {
     setSorting(!sorting)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+    let obj = {
+      description: e.target[0].value,
+      image: e.target[1].value,
+      location: e.target[2].value
+    }
+    fetch('http://localhost:6001/listings', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(obj)
+    })
+    .then((res) => res.json())
+    .then((newItem) => setDataList([...dataList, newItem]))
+  }
+
   return (
     <main>
       <button onClick={handleSortClick}>Sort by Location</button>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='description'/>
+          <input type='text' placeholder='image'/>
+          <input type='text' placeholder='location'/>
+          <input type='submit' />
+        </form>
+      </div>
       <ul className="cards">
         {dataList.map((item) => {
           return (
